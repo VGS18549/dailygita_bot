bc10b072371fd4740fc04b7be8acec9f219d3d68
Vanga Ganesh Sai Kartheek <107295767+VGS18549@users.noreply.github.com>
2022-10-07 21:08:09 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import const as keys
+from telegram.ext import *
+import responses as r
+
+
+print("BOT Started ............")
+def start_command(update,context):
+    update.message.reply_text("VASUDEVA")
+
+
+def handle_message(update, context):
+    text= str(update.message.text).lower()
+    response = r.sample_responses(text)
+    update.message.reply_text(response)
+
+
+def error(update,context):
+    print(f"Update{update} caused error{context.error}")
+
+
+
+def main():
+    updater = Updater(keys.API_key,use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start_command))
+
+    dp.add_handler(MessageHandler(Filters.text,handle_message))
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+main()

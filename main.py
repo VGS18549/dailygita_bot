dd1134e21c22dfc349943e403c6b89eda4359a95
Vanga Ganesh Sai Kartheek <107295767+VGS18549@users.noreply.github.com>
2023-07-26 21:58:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,71 @@
+from typing import Final
+# pip install python-telegram-bot
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+import responses
 import const as keys
-from telegram.ext import *
-import responses as r
+
+print('Starting up bot...')
+TOKEN: Final = keys.API_key
+BOT_USERNAME: Final = '@dailygita_bot'
 
 
-print("BOT Started ............")
-def start_command(update,context):
-    update.message.reply_text("VASUDEVA")
+# Lets us use the /start command
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("हरे कृष्ण हरे कृष्ण, कृष्ण कृष्ण हरे हरे |\nहरे राम हरे राम, राम राम हरे हरे ||")
 
 
-def handle_message(update, context):
-    text= str(update.message.text).lower()
-    response = r.sample_responses(text)
-    update.message.reply_text(response)
+def handle_response(text: str) -> str:
+    # Create your own response logic
+    processed: str = text.lower()
+
+    if 'vasudeva' or 'krishna' or 'vishnu' or 'goplala' or "hari" in processed:
+        x = responses.sample_responses("/vasudeva")
+        return x
+    return "Type vasudea / krishna / gopala / hari"
 
 
-def error(update,context):
-    print(f"Update{update} caused error{context.error}")
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # Get basic info of the incoming message
+    message_type: str = update.message.chat.type
+    text: str = update.message.text
+    # Print a log for debugging
+    print(f'User ({update.message.chat.id}) in {message_type}: "{text}"')
+
+    # React to group messages only if users mention the bot directly
+    if message_type == 'group':
+        # Replace with your bot username
+        if BOT_USERNAME in text:
+            new_text: str = text.replace(BOT_USERNAME, '').strip()
+            response: str = handle_response(new_text)
+        else:
+            return  # We don't want the bot respond if it's not mentioned in the group
+    else:
+        response: str = handle_response(text)
+
+    # Reply normal if the message is in private
+    # print('Bot:', response)
+    await update.message.reply_text(response)
 
 
+# Log errors
+async def error(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(f'Update {update} caused error {context.error}')
 
-def main():
-    updater = Updater(keys.API_key,use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start_command))
 
-    dp.add_handler(MessageHandler(Filters.text,handle_message))
-    dp.add_error_handler(error)
+# Run the program
+if __name__ == '__main__':
+    app = Application.builder().token(TOKEN).build()
 
-    updater.start_polling()
-    updater.idle()
+    # Commands
+    app.add_handler(CommandHandler('start', start_command))
 
-main()
+    # Messages
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+
+    # Log all errors
+    app.add_error_handler(error)
+
+    print('Polling...')
+    # Run the bot
+    app.run_polling(poll_interval=5)
